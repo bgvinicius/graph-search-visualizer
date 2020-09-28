fd25c2eed185375f9649122ca8ad5fc3da3158de
Rodrigo <thenogrodrigo1@gmail.com>
2020-09-28 12:02:45 -0300

    Classe com funções úteis e genéricas

---
+++ src/models/Util.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.util.*;
+
+public class Util {
+    public static Object[] shuffle(Object[] array) {
+        List<Obect[]> list = new ArrayList<>();
+        
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+
+        Collections.shuffle(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            array[0] = list.get(i);
+        }
+    }
+}
